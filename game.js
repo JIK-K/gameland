ac4f0dbb288f9ef7a52b0e615990a753706639ab
abcdaramgi <wwa1102@naver.com>
2022-04-28 13:12:18 +0900

    저는 이제 두번이나 뛸 수 있어요

--- game.js
+++ game.js
@@ -75,6 +75,9 @@ const keys = {
     }
 }
 
+let isGround = false;
+let doubleJump = false;
+let jumpCount = 2;
 
 function animate(){
     window.requestAnimationFrame(animate)
@@ -86,12 +89,46 @@ function animate(){
     player.velocity.x = 0
     
     if(keys.right.pressed && player.lastkey === 'ArrowRight'){
-        player.velocity.x = 5
+        player.velocity.x = 10
     } else if (keys.left.pressed && player.lastkey === 'ArrowLeft'){
-        player.velocity.x = -5
+        player.velocity.x = -10
     } /*else if (keys.up.pressed && player.lastkey === 'ArrowUp'){
         player.velocity.y = -10
     } */
+
+    /*
+    if(player.velocity.y == 0){
+        isGround = true;
+    } else {
+        isGround = false;
+    }
+
+    if(isGround){
+        doubleJump = true;
+    }
+
+    if(isGround && keys.up.pressed){
+        player.velocity.y = -20
+    } else if(doubleJump && keys.up.pressed){
+        player.velocity.y = -20
+        doubleJump = false;
+    }
+    */
+
+    if(player.velocity.y == 0){
+        isGround = true;
+    } else {
+        isGround = false;
+    }
+    if(isGround){
+        jumpCount = 2;
+    }
+
+/*
+    if(keys.up.pressed && player.position.y > 426){
+        player.velocity.y = -20
+    }
+*/
 }
 
 animate()
@@ -107,8 +144,12 @@ window.addEventListener('keydown', (event) => {
             player.lastkey = 'ArrowLeft'
             break
         case 'ArrowUp':
-            player.velocity.y = -10
-            //keys.up.pressed = true
+        if(jumpCount > 0){
+                player.velocity.y = -15
+                jumpCount--;
+                console.log(jumpCount);
+        }
+            keys.up.pressed = true
             //player.lastkey = 'ArrowUp'
             break
     }
@@ -124,7 +165,7 @@ window.addEventListener('keyup', (event) => {
             keys.left.pressed = false
             break
         case 'ArrowUp':
-            //keys.up.pressed = false
+            keys.up.pressed = false
             break
     }
 })
