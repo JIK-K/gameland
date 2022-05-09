42f25ef285e2af33f951cf1fdc31fafb3d020008
abcdaramgi <wwa1102@naver.com>
2022-05-09 12:51:07 +0900

    너무너무 느려요!

--- game.js
+++ game.js
@@ -9,20 +9,45 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const gravity = 0.7
 
 class Sprite{
-    constructor({position, velocity}){
+    constructor({position, velocity, color = 'red', offset}){
         this.position = position
         this.velocity = velocity
         this.height = 150
+        this.width = 50
         this.lastkey
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset: offset,
+            width: 100,
+            height: 50,
+        }
+        this.color = color
+        this.isAttacking
     }
 
     draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        //attackBox
+        if(this.isAttacking) {
+        c.fillStyle = 'green'
+        c.fillRect(
+            this.attackBox.position.x, 
+            this.attackBox.position.y, 
+            this.attackBox.width, 
+            this.attackBox.height
+            )
+        }
     }
 
     update() {
         this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y
 
         this.position.y += this.velocity.y
         this.position.x += this.velocity.x
@@ -31,6 +56,14 @@ class Sprite{
             this.velocity.y = 0
         } else 
         this.velocity.y += gravity
+    }
+
+    attack() {
+        this.isAttacking = true
+        //이런 문법 어떻게 쓰는건지 아직 잘 모르겠다
+        setTimeout(() => {
+            this.isAttacking = false
+        }, 100)
     }
 }
 
@@ -42,6 +75,10 @@ const player = new Sprite({
     velocity: {
         x: 0,
         y: 0
+    },
+    offset: {
+        x: 0,
+        y: 0
     }
 })
 
@@ -49,12 +86,17 @@ player.draw()
 
 const enemy = new Sprite({
     position: {
-    x: 400,
-    y: 100
+        x: 400,
+        y: 100
     },
     velocity: {
-    x: 0,
-    y: 0
+        x: 0,
+        y: 0
+    },
+    color: 'blue',
+    offset: {
+        x: -50,
+        y: 0
     }
 })
 
@@ -73,6 +115,14 @@ const keys = {
     up: {
         pressed: false
     }
+}
+
+function rectangularCollision ({ rectangle1, rectangle2}) {
+    return (rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
+            rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+            rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+            rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    )
 }
 
 let isGround = false;
@@ -99,12 +149,12 @@ function animate(){
         enemy.velocity.x = -1
         if(disyPtE < -100){
            enemy.velocity.y = -10
-    }
+        }
     } else if (disxPtE < 300 && disxPtE > 0){
         enemy.velocity.x = 1
         if(disyPtE < -100){
            enemy.velocity.y = -5
-    }
+        }
     }
 
 
@@ -145,6 +195,26 @@ function animate(){
         jumpCount = 2;
     }
 
+    //detect for collision
+    if( rectangularCollision({
+        rectangle1: player,
+        rectangle2: enemy
+    }) &&
+        player.isAttacking
+    ) {
+        player.isAttacking = false
+        document.querySelector('#enemyHealth').style.width = '20%'
+    }
+
+    if( rectangularCollision({
+        rectangle1: enemy,
+        rectangle2: player
+    }) &&
+        enemy.isAttacking
+        ) {
+        enemy.isAttacking = false
+        console.log('enemy attack successful');
+    }
 /*
     if(keys.up.pressed && player.position.y > 426){
         player.velocity.y = -20
@@ -172,8 +242,15 @@ window.addEventListener('keydown', (event) => {
             keys.up.pressed = true
             //player.lastkey = 'ArrowUp'
             break
+        case ' ':
+            player.attack()
+            break
     }
-    
+    switch (event.key){
+        case 's':
+            enemy.attack()
+            break
+    }
 })
 
 window.addEventListener('keyup', (event) => {
