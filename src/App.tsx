e1546271c6473727823b65f797c4202cee5e981b
abcdaramgi <wwa1102@naver.com>
2024-01-02 14:01:17 +0900

    아름다운 예수님~ 금수강산에

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Router from "./components/Router";
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          가즈아 <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
+function App() {
+  return <Router />;
+}
 
 export default App;
---
+++ src/components/Router.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutPage from "../pages/About/AboutPage";
+import Home from "../pages/Home/HomePage";
+import React from "react";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
