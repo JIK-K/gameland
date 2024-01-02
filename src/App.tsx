bcbc80a48a0ea3f5db4f26f30a6171bc24813669
abcdaramgi <wwa1102@naver.com>
2024-01-02 15:39:51 +0900

    ㅋㅋㅄㅅㄲ

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Home from "./pages/Home/HomePage";
 
 function App() {
   return (
-    <BrowserRouter basename="https://jik-k.github.io/gameland/">
-      <div>
-        ddsdfgsdfg
-        <div>dddd</div>
-        <NavLink to="/">Home</NavLink>
-      </div>
-      <Route path="/home" Component={Home} />
-    </BrowserRouter>
+    <div>
+      ddsdfgsdfg
+      <div>dddd</div>
+      <NavLink to="/home">Home</NavLink>
+    </div>
   );
 }
 
