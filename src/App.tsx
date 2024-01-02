b619f0dd5467630936662d56d15ad4730166bbbc
abcdaramgi <wwa1102@naver.com>
2024-01-02 14:47:00 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import "./App.css";
-import Router from "./components/Router";
+import AboutPage from "./pages/About/AboutPage";
+import HomePage from "./pages/Home/HomePage";
+import JustPage from "./pages/Just/JustPage";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
 const App = () => {
-  return <Router />;
+  return (
+    <BrowserRouter basename="https://jik-k.github.io/GameLand">
+      <Routes>
+        <NavLink to="/home">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <Route path="/" element={<JustPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
