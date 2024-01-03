8ccd46addf5b6feac72ce7267f5a34d402560988
Jung Woon Jik <dnswlrsla@gmail.com>
2024-01-03 15:17:53 +0900

    하기귀찮아졌다

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import Home from "./pages/Home/HomePage";
 function App() {
   return (
     <div>
-      ddsdfgsdfg
-      <div>dddd</div>
       <NavLink to="/home">Home</NavLink>
       <NavLink to="/about">About</NavLink>
     </div>
