ed8e8542be57552c64a8bda139e5715a5709fc08
Jung Woon Jik <dnswlrsla@gmail.com>
2024-01-19 15:57:18 +0900

    Update 천공대전.tsx

--- src/pages/Games/천공대전/천공대전.tsx
+++ src/pages/Games/천공대전/천공대전.tsx
@@ -13,7 +13,7 @@ const 천공대전 = () => {
     <Background>
       <TopBar />
       <iframe
-        src="https://www.miniplay.com/helpers/game/sandbox.php?mp_assets=https%3A%2F%2Fs2.minijuegosgratis.com%2F&mp_embed=0&mp_game_id=2285&mp_game_uid=etherena&mp_game_url=https%3A%2F%2Fwww.miniplay.com%2Fembed%2Fetherena&mp_int=1&mp_locale=en_US&mp_player_type=FLASH&mp_player_url=aHR0cHM6Ly93d3cubWluaWp1ZWdvc2dyYXRpcy5jb20vZmxhc2gxMjM0L25vX2hvdGxpbmsvZXRoZXJlbmEuc3dm&mp_site_https_url=https%3A%2F%2Fwww.miniplay.com%2F&mp_site_name=miniplay.com&mp_site_url=https%3A%2F%2Fwww.miniplay.com%2F&mp_timezone=Asia%2FSeoul&mp_view_type="
+        src="https://www.miniplay.com/embed/etherena"
         title="천공대전"
         width="100%"
         height="100%"
